c8c1c55048397baacbe892ae98d8e8aea08c5122
Liu <13032975+L_Chrng@user.noreply.gitee.com>
2023-07-21 17:48:00 +0800

    feat: ✨ 尝试使用hook函数

---
+++ src/views/HookDemo.vue
@@ -0,0 +1,9 @@
+<template>useCharts</template>
+<script setup lang="ts">
+  import { onMounted } from "vue";
+  const test = () => {
+    console.log("onMounted");
+  };
+  onMounted(test);
+</script>
+<style scoped lang="scss"></style>
