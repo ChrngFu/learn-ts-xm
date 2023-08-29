914eea7d786b246f6337d393b666eeb9a5a39529
Liu <13032975+L_Chrng@user.noreply.gitee.com>
2023-08-29 18:01:43 +0800

    打包

--- build/clear.js
+++ build/clear.js
@@ -1,6 +1,6 @@
-import fs from "fs";
+const fs = require("fs");
 
-const filePath = "../docs";
+const filePath = "../../docs";
 
 if (fs.existsSync(filePath)) {
   const files = fs.readdirSync(filePath);
